b4ab80ab4d265b5f68b1744dbf68043c19129db3
Oksana Makarchuk <oksanamakarchuk@gmail.com>
2019-03-22 13:28:06 +0200

    Lesson 11

--- src/test/java/page/HomePage.java
+++ src/test/java/page/HomePage.java
@@ -8,8 +8,10 @@ import org.openqa.selenium.support.PageFactory;
 
 import static java.lang.Thread.sleep;
 
-public class HomePage {
-    private WebDriver driver;
+/**
+ * Page Object class for HomePage
+ */
+public class HomePage extends BasePage {
 
     @FindBy(xpath = "//Li[@id='profile-nav-item']")
     private WebElement profileNavMenuItem;
@@ -17,17 +19,30 @@ public class HomePage {
     @FindBy (xpath = "//form[@id='extended-nav-search']//input")
     private WebElement searchField;
 
+    /**
+     * Constructor for HomePage class
+     * @param driver
+     */
     public HomePage(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(driver, this);
     }
 
+    /**
+     * Method which checks if the page is loaded
+     * @return
+     */
     public boolean isPageLoaded() {
         return profileNavMenuItem.isDisplayed()
                 && driver.getCurrentUrl().contains("/feed/")
                 && driver.getTitle().contains("LinkedIn");
     }
 
+    /**
+     * Method which helps to search an item
+     * @param searchTerm
+     * @return
+     */
     public SearchPage search(String searchTerm) {
         searchField.sendKeys(searchTerm);
         searchField.sendKeys(Keys.ENTER);
